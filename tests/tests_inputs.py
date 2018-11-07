6f2e071ba640331f467a55237921311834886132
Sidnei Pereira <sidi.jp@gmail.com>
2018-11-07 20:11:00 -0200

    Add tests for Input validators.

--- tests/tests_inputs.py
+++ tests/tests_inputs.py
@@ -48,3 +48,80 @@ class TestInputScanner(unittest.TestCase):
         )
 
         self.assertIs(next(scanner), None)
+
+
+class TestInputEqualsValidator(unittest.TestCase):
+    def setUp(self):
+        self.values = [-1, 0, 10000, 999]
+        self.expected_values = zip(self.values, self.values)
+        self.unexpected_values = zip(self.values, [-2, 1, -100, 99])
+
+    def test_expected_values(self):
+        for v, e in self.expected_values:
+            validator = InputEqualsValidator(v)
+            self.assertTrue(validator.validate(e))
+
+    def test_unexpected_values(self):
+        for v, u in self.unexpected_values:
+            validator = InputEqualsValidator(v)
+            self.assertFalse(validator.validate(u))
+
+
+class TestInputInclusiveRangeValidator(unittest.TestCase):
+    def setUp(self):
+        self.lower = 10
+        self.upper = 100
+        self.validator = InputInclusiveRangeValidator(self.lower, self.upper)
+
+    def test_value_inside_range(self):
+        value = (self.lower + self.upper) / 2
+        self.assertTrue(self.validator.validate(value))
+
+    def test_value_in_range_limits(self):
+        value = self.lower
+        self.assertTrue(self.validator.validate(value))
+        value = self.upper
+        self.assertTrue(self.validator.validate(value))
+
+    def test_value_out_of_range(self):
+        value = self.lower - 1
+        self.assertFalse(self.validator.validate(value))
+        value = self.upper + 1
+        self.assertFalse(self.validator.validate(value))
+
+
+class TestInputIntegerValidator(unittest.TestCase):
+    def setUp(self):
+        self.validator = InputIntegerValidator()
+
+    def test_is_integer(self):
+        self.assertIsInstance(0, int)
+
+    def test_is_not_integer(self):
+        self.assertFalse(isinstance('a', int))
+        self.assertFalse(isinstance(1.0, int))
+        self.assertFalse(isinstance([], int))
+        self.assertFalse(isinstance(tuple(), int))
+        self.assertFalse(isinstance({}, int))
+
+
+class TestInputOrValidator(unittest.TestCase):
+    def test_all_validators_true(self):
+        validator = InputOrValidator([
+            InputIntegerValidator(),
+            InputEqualsValidator(0),
+            InputInclusiveRangeValidator(0, 10),
+        ])
+        value = 0
+        self.assertTrue(validator.validate(value))
+
+    def test_none_validators_false(self):
+        validator = InputOrValidator([
+            InputEqualsValidator(0),
+            InputInclusiveRangeValidator(0, 10),
+        ])
+        value = '-1'
+        self.assertFalse(validator.validate(value))
+
+    def test_any_validators_true(self):
+        pass
